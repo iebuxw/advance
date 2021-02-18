de5ae074698310ee0bac2cdf0b5d30ceb1c60467
xiaowang <xiaowang@qianxin.com>
2021-02-18 10:58:03 +0800

    1

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -77,6 +77,7 @@ class SiteController extends Controller
         $this->layout = 'blank';
 
         $model = new LoginForm();
+
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
         } else {
