a3c58c325bd2d6e27eb1120ec0cd32aa42db3e88
xiaowang <xiaowang@qianxin.com>
2021-03-31 10:29:49 +0800

    过滤器filters

--- frontend/controllers/ExcerptController.php
+++ frontend/controllers/ExcerptController.php
@@ -43,6 +43,10 @@ class ExcerptController extends Controller
                     ],
                 ],
             ],
+            'ActionTimeFilter' => [
+                'class' => 'frontend\components\ActionTimeFilter'
+
+            ],
         ];
     }
 
